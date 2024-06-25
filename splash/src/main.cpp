a795b2f61569a9a307d41beb10ca0ce8286e7195
Rein van der Woerd <reinvanderwoerd@me.com>
2024-06-25 10:14:57 +0200

    splash: refactored to make code shorter, unsuccesful attempt at higher-res version

--- splash/src/main.cpp
+++ splash/src/main.cpp
@@ -31,6 +31,24 @@
 
 #include "bn_music_items.h"
 
+#include <vector>
+
+
+
+bn::sprite_animate_action<81> splash_action(const bn::sprite_ptr &sprite, const bn::sprite_item &sprite_item) 
+{
+    bn::sprite_animate_action<81> splash_action = bn::create_sprite_animate_action_forever(sprite, 1, sprite_item.tiles_item(), 
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 
+        16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 
+        32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47,
+        48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63,
+        64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 
+        80
+    );
+
+    return splash_action;
+}
+
 
 
 int main()
@@ -40,93 +58,63 @@ int main()
     bn::sprite_text_generator text_generator(common::variable_8x16_sprite_font);
     bn::bg_palettes::set_transparent_color(bn::color(0, 0, 0));
 
+    // High-res version
+    // int x_offset = 0;
+    // int y_offset = 32;
+    // bn::sprite_ptr splash_1 = bn::sprite_items::splash_1.create_sprite(-64+x_offset, -64+y_offset);
+    // bn::sprite_ptr splash_2 = bn::sprite_items::splash_2.create_sprite(0+x_offset, -64+y_offset);
+    // bn::sprite_ptr splash_3 = bn::sprite_items::splash_3.create_sprite(64+x_offset, -64+y_offset);
+
+    // bn::sprite_ptr splash_4 = bn::sprite_items::splash_4.create_sprite(-64+x_offset, 0+y_offset);
+
+    // bn::sprite_ptr splash_5 = bn::sprite_items::splash_5.create_sprite(0+x_offset, 0+y_offset);
+    // bn::sprite_ptr splash_6 = bn::sprite_items::splash_6.create_sprite(64+x_offset, 0+y_offset);
+
+
+    // Low-res version 
     int x_offset = 10;
     int y_offset = 15;
 
-    bn::sprite_ptr splash_1 = bn::sprite_items::splash_1.create_sprite(-64+x_offset, -64+y_offset);
-    bn::sprite_ptr splash_2 = bn::sprite_items::splash_2.create_sprite(64+x_offset, -64+y_offset);
 
-    bn::sprite_ptr splash_3 = bn::sprite_items::splash_3.create_sprite(-64+x_offset, 0+y_offset);
-    bn::sprite_ptr splash_4 = bn::sprite_items::splash_4.create_sprite(64+x_offset, 0+y_offset);
+    bn::sprite_item sprite_items[] {
+        bn::sprite_items::splash_1,
+        bn::sprite_items::splash_2,
+        bn::sprite_items::splash_3,
+        bn::sprite_items::splash_4,
+        bn::sprite_items::splash_5,
+        bn::sprite_items::splash_6
+    };
 
-    bn::sprite_ptr splash_5 = bn::sprite_items::splash_5.create_sprite(-64+x_offset, 64+y_offset);
-    bn::sprite_ptr splash_6 = bn::sprite_items::splash_6.create_sprite(64+x_offset, 64+y_offset);
-    
+    bn::sprite_ptr splash_sprite_ptrs[] {
+        bn::sprite_items::splash_1.create_sprite(-64+x_offset, -64+y_offset),
+        bn::sprite_items::splash_2.create_sprite(64+x_offset, -64+y_offset),
+        bn::sprite_items::splash_3.create_sprite(-64+x_offset, 0+y_offset),
+        bn::sprite_items::splash_4.create_sprite(64+x_offset, 0+y_offset),
+        bn::sprite_items::splash_5.create_sprite(-64+x_offset, 64+y_offset),
+        bn::sprite_items::splash_6.create_sprite(64+x_offset, 64+y_offset)
+    };
 
-    splash_1.set_scale(2);
-    splash_2.set_scale(2);
-    splash_3.set_scale(2);
-    splash_4.set_scale(2);
-    splash_5.set_scale(2);
-    splash_6.set_scale(2);
+    bn::sprite_animate_action<81> splash_anim_actions[] {
+        splash_action(splash_sprite_ptrs[0], sprite_items[0]),
+        splash_action(splash_sprite_ptrs[1], sprite_items[1]),
+        splash_action(splash_sprite_ptrs[2], sprite_items[2]),
+        splash_action(splash_sprite_ptrs[3], sprite_items[3]),
+        splash_action(splash_sprite_ptrs[4], sprite_items[4]),
+        splash_action(splash_sprite_ptrs[5], sprite_items[5])
+    };
 
-    
-    bn::sprite_animate_action<81> splash_1_action = bn::create_sprite_animate_action_forever(splash_1, 1, bn::sprite_items::splash_1.tiles_item(), 
-        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 
-        16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 
-        32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47,
-        48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63,
-        64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 
-        80
-    );
 
-    bn::sprite_animate_action<81> splash_2_action = bn::create_sprite_animate_action_forever(splash_2, 1, bn::sprite_items::splash_2.tiles_item(), 
-        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 
-        16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 
-        32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47,
-        48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63,
-        64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 
-        80
-    );
-
-    bn::sprite_animate_action<81> splash_3_action = bn::create_sprite_animate_action_forever(splash_3, 1, bn::sprite_items::splash_3.tiles_item(), 
-        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 
-        16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 
-        32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47,
-        48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63,
-        64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 
-        80
-    );
-
-    bn::sprite_animate_action<81> splash_4_action = bn::create_sprite_animate_action_forever(splash_4, 1, bn::sprite_items::splash_4.tiles_item(), 
-        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 
-        16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 
-        32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47,
-        48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63,
-        64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 
-        80
-    );
-
-    bn::sprite_animate_action<81> splash_5_action = bn::create_sprite_animate_action_forever(splash_5, 1, bn::sprite_items::splash_5.tiles_item(), 
-        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 
-        16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 
-        32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47,
-        48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63,
-        64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 
-        80
-    );
-
-    bn::sprite_animate_action<81> splash_6_action = bn::create_sprite_animate_action_forever(splash_6, 1, bn::sprite_items::splash_6.tiles_item(), 
-        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 
-        16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 
-        32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47,
-        48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63,
-        64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 
-        80
-    );
+    for (size_t i = 0; i < 6; i++) {
+        splash_sprite_ptrs[i].set_scale(2);
+    }
 
     bn::music_items::breakagez.play();
 
-
-
     while(true)
     {
-        splash_1_action.update();
-        splash_2_action.update();
-        splash_3_action.update();
-        splash_4_action.update();
-        splash_5_action.update();
-        splash_6_action.update();
+        for (size_t i = 0; i < 6; i++) {
+            splash_anim_actions[i].update();
+        }
 
         bn::core::update();
     }
