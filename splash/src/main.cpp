f4636b46ae5737d77ec3ee3f07f3f63d3883bf6a
Rein van der Woerd <reinvanderwoerd@me.com>
2024-07-10 18:14:50 +0000

    [splash] removed some includes

--- splash/src/main.cpp
+++ splash/src/main.cpp
@@ -2,26 +2,19 @@
 #include "bn_bg_palettes.h"
 #include "bn_music.h"
 #include "bn_log.h"
-#include "bn_regular_bg_ptr.h"
-#include "bn_regular_bg_item.h"
-#include "bn_regular_bg_map_ptr.h"
 
 #include "splash.h"
 
 #include "bn_music_items.h"
-#include "bn_regular_bg_items_zwembad.h"
-
 
 
 int main()
 {
     bn::core::init();
-    bn::bg_palettes::set_transparent_color(bn::color(0, 0, 0));
+    bn::bg_palettes::set_transparent_color(bn::color(31, 0, 31));
     bn::music_items::splashscreen.play();
 
     splash::setup();
-
-    // bn::regular_bg_ptr zwembad = bn::regular_bg_items::zwembad.create_bg(8,50);
 
     while(true)
     {
